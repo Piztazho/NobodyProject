cd365aa565a142505edbc852cfe797d574cba7df
Ricardo Quiñonez <victor.quinonez.paez@accenture.com>
2017-07-18 22:22:20 -0500

    add owl.carousel

--- nobody-photo/src/app/home/home.component.ts
+++ nobody-photo/src/app/home/home.component.ts
@@ -8,12 +8,13 @@ import { DataService } from '../services/data.service';
 })
 export class HomeComponent implements OnInit {
 
-  	constructor( private _service: DataService ) { }
+  	constructor( private _service: DataService ) {	}
 
   	ngOnInit() {
   		this._service.getData('projects').then( r =>{
   			console.log(r.json());
   		});
+
   	}
 
 }
